7b35d81a54e9109a4e7cac9d04fd2691f1748370
Javier Combita Tellez <javiercombita2014@gmail.com>
2025-01-27 14:20:29 -0500

    Feat: el proyecto base fue creado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CRUD_MVC_users.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
