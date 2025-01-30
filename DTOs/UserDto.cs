8731465d53a6b8bde2710a327e96502bec54cff7
Javier Combita Tellez <javiercombita2014@gmail.com>
2025-01-29 21:21:57 -0500

    Fix: se creo el dto de usuario

---
+++ DTOs/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CRUD_MVC_users.DTOs;
+
+public class UserDto
+{
+    public string name { get; set; }
+    public string lastname { get; set; }
+    public string email { get; set; }
+    public DateOnly dateBirth { get; set; }
+    public string gender { get; set; }
+}
