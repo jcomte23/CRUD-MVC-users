f33e52bad0e652c443d9e40e6ee6c3ec494e8cd4
Javier Combita Tellez <javiercombita2014@gmail.com>
2025-01-31 22:24:29 -0500

    Fix: se mejoraron los mensajes del DTO user

--- DTOs/UserDto.cs
+++ DTOs/UserDto.cs
@@ -4,24 +4,24 @@ namespace CRUD_MVC_users.DTOs;
 
 public class UserDto
 {
-    [Required(ErrorMessage = "El nombre es obligatorio.")]
-    [StringLength(50, MinimumLength = 2, ErrorMessage = "El nombre debe tener entre 2 y 50 caracteres.")]
+    [Required(ErrorMessage = "The name is required.")]
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "The name must be between 2 and 50 characters.")]
     public string Name { get; set; }
 
-    [Required(ErrorMessage = "El apellido es obligatorio.")]
-    [StringLength(50, MinimumLength = 2, ErrorMessage = "El apellido debe tener entre 2 y 50 caracteres.")]
+    [Required(ErrorMessage = "The last name is required.")]
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "The last name must be between 2 and 50 characters.")]
     public string Lastname { get; set; }
 
-    [Required(ErrorMessage = "El correo electrónico es obligatorio.")]
-    [EmailAddress(ErrorMessage = "El formato del correo no es válido.")]
+    [Required(ErrorMessage = "The email is required.")]
+    [EmailAddress(ErrorMessage = "The email format is not valid.")]
     public string Email { get; set; }
 
-    [Required(ErrorMessage = "La fecha de nacimiento es obligatoria.")]
+    [Required(ErrorMessage = "The date of birth is required.")]
     [DataType(DataType.Date)]
-    [Range(typeof(DateOnly), "1900-01-01", "2024-12-31", ErrorMessage = "La fecha de nacimiento debe estar entre 1900 y 2024.")]
+    [Range(typeof(DateOnly), "1900-01-01", "2024-12-31", ErrorMessage = "The date of birth must be between 1900 and 2024.")]
     public DateOnly DateBirth { get; set; }
 
-    [Required(ErrorMessage = "El género es obligatorio.")]
-    [RegularExpression("^(M|F)$", ErrorMessage = "El género debe ser 'M' (Masculino) o 'F' (Femenino).")]
+    [Required(ErrorMessage = "The gender is required.")]
+    [RegularExpression("^(M|F)$", ErrorMessage = "The gender must be 'M' (Male) or 'F' (Female).")]
     public string Gender { get; set; }
 }
