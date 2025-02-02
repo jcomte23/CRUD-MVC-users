5c5808f90f9134f7f426ece4a94351adcdd519e3
Javier Combita Tellez <javiercombita2014@gmail.com>
2025-02-01 20:24:14 -0500

    Feat: se renderizaron los datos desde la base de datos

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,12 +1,22 @@
+using CRUD_MVC_users.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CRUD_MVC_users.Controllers;
 
 public class UsersController : Controller
 {
-    // GET
-    public IActionResult Index()
+    private readonly ApplicationDbContext _context;
+
+    public UsersController(ApplicationDbContext context)
     {
-        return View();
+        _context = context;
+    }
+    
+    public async Task<IActionResult> Index()
+    {
+        var users = await _context.Users.ToListAsync();
+        
+        return View(users);
     }
 }
